016a01ec6f5c134318b424f086571270c73e87ec
Oliver de Sousa <oliver.seq@gmail.com>
2018-08-30 08:44:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,19 +10,39 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(Math.random() * 100);
+  cart.push({itemName:`${item}`, itemPrice:price});
+  return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0){ return "Your shopping cart is empty."
+  }
+  else if (cart.length === 1){ return "In your cart, you have " +cart[0].itemName+ " at $" +cart[0].itemPrice+ ".";
+  }
+  //I got really stuck here...until taught by one of the coaches that to call an object key/value you could use the code shown above, (cart[0].itemName). With this I was able to solve the function.
+  else if (cart.length === 2){ return "In your cart, you have " +cart[0].itemName+ " at $" +cart[0].itemPrice+ ", and " +cart[1].itemName+ " at $" +cart[1].itemPrice+".";
+  }
+  else if (cart.length === 3){ return "In your cart, you have " +cart[0].itemName+ " at $" +cart[0].itemPrice+ ", " +cart[1].itemName+ " at $" +cart[1].itemPrice+ ", and " +cart[2].itemName+ " at $" +cart[2].itemPrice+".";
+  }
 }
 
 function total() {
-  // write your code here
+  var sum = 0;
+  for (var i = 0; i < cart.length; i++){
+    sum = sum + cart[i].itemPrice;
+  }
+  return sum;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for (var i = 0; i < cart.length; i++){
+    if (item === cart[i].itemName){
+      cart.splice(0,[i],item);
+      return cart;
+    }
+    else return "That item is not in your cart.";
+  }
 }
 
 function placeOrder(cardNumber) {
